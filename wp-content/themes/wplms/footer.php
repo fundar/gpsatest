a7336a4f8d6051d1811b340d45eb3e476ed9c48c
CeciliaMadrid <lya.madrid@gmail.com>
2014-09-26 14:23:58 -0500

    acento footer bottom

--- wp-content/themes/wplms/footer.php
+++ wp-content/themes/wplms/footer.php
@@ -106,7 +106,7 @@
         <div class="row">
             <div class="col-md-8">              
                 <?php $copyright=vibe_get_option('copyright'); echo (isset($copyright)?$copyright:'&copy; 2013, All rights reserved.'); ?>, Developed by 
-                <a href="http://fundar.org.mx"  style="text-decoration:none;" title="Fundar, Centro de Análisis e Investigación">Fundar, Centro de An&aacute;lisis e Investigaci6oacute;n</a>
+                <a href="http://fundar.org.mx"  style="text-decoration:none;" title="Fundar, Centro de Análisis e Investigación">Fundar, Centro de An&aacute;lisis e Investigaci&oacute;n</a>
             </div>
             <div class="col-md-4">
                 <div id="footermenu">
