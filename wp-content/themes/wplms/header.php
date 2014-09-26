956289760a6f4b12b7331d80acd8251b8e05ddef
CeciliaMadrid <lya.madrid@gmail.com>
2014-09-26 09:36:20 -0500

    header costum

--- wp-content/themes/wplms/header.php
+++ wp-content/themes/wplms/header.php
@@ -3,35 +3,35 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="/favicon.ico">
 <title>
-<?php echo wp_title('|',true,'right'); ?>
+<?php 
+echo get_the_title();
+//wp_title('|',true,'right'); 
+?>
 </title>
 <?php
-
-$layout = vibe_get_option('layout');
-if(!isset($layout) || !$layout)
-    $layout = '';
-
 wp_head();
 ?>
+
 </head>
-<body <?php body_class($layout); ?>>
+<body <?php body_class(); ?>>
 <div id="global" class="global">
     <div class="pagesidebar">
         <div class="sidebarcontent">    
             <h2 id="sidelogo">
-            <a href="<?php echo vibe_site_url(); ?>"><img src="<?php  echo apply_filters('wplms_logo_url',VIBE_URL.'/images/logo.png'); ?>" alt="<?php echo get_bloginfo('name'); ?>" /></a>
+            <a href="<?php vibe_site_url(); ?>"><img src="<?php $logo=vibe_get_option('logo'); echo (isset($logo)?$logo:VIBE_URL.'/images/logo.png'); ?>" /></a>
             </h2>
             <?php
-                $args = apply_filters('wplms-mobile-menu',array(
+                $args = array(
                     'theme_location'  => 'mobile-menu',
                     'container'       => '',
                     'menu_class'      => 'sidemenu',
                     'fallback_cb'     => 'vibe_set_menu',
-                ));
-
+                );
                 wp_nav_menu( $args );
             ?>
         </div>
@@ -45,14 +45,14 @@ wp_head();
             <div class="container">
                 <div class="row">
                     <div class="col-md-4 col-sm-3">
-                       <a href="<?php echo vibe_site_url(); ?>" class="homeicon"><img src="<?php  echo apply_filters('wplms_logo_url',VIBE_URL.'/images/logo.png'); ?>" alt="<?php echo get_bloginfo('name'); ?>" /></a> 
+                       <a href="<?php echo vibe_site_url(); ?>" class="homeicon"><img src="<?php echo (isset($logo)?$logo:VIBE_URL.'/images/logo.png'); ?>" alt="<?php echo get_bloginfo('name'); ?>"></a>
                     </div>
                     <div class="col-md-8 col-sm-9">
                     <?php
                     if ( function_exists('bp_loggedin_user_link') && is_user_logged_in() ) :
                         ?>
                         <ul class="topmenu">
-                            <li><a href="<?php bp_loggedin_user_link(); ?>" class="smallimg vbplogin"><?php $n=vbp_current_user_notification_count(); echo ((isset($n) && $n)?'<em></em>':''); bp_loggedin_user_avatar( 'type=full' ); ?><?php bp_loggedin_user_fullname(); ?></a></li>
+                            <li><a href="<?php bp_loggedin_user_link(); ?>" class="smallimg vbplogin"><?php bp_loggedin_user_avatar( 'type=full' ); ?><?php bp_loggedin_user_fullname(); ?></a></li>
                         </ul>
                     <?php
                     else :
@@ -60,20 +60,14 @@ wp_head();
                         <ul class="topmenu">
                             <li><a href="#login" class="smallimg vbplogin"><?php _e('Login','vibe'); ?></a></li>
                             <li><?php if ( function_exists('bp_get_signup_allowed') && bp_get_signup_allowed() ) :
-                                printf( __( '<a href="%s" class="vbpregister" title="'.__('Create an account','vibe').'">'.__('Sign Up','vibe').'</a> ', 'vibe' ), site_url( BP_REGISTER_SLUG . '/' ) );
+                                printf( __( '<a href="%s" class="vbpregister" title="Create an account">Sign Up</a> ', 'vibe' ), site_url( BP_REGISTER_SLUG . '/' ) );
                             endif; ?>
                             </li>
                         </ul>
+                    
                     <?php
                     endif;
-                            $args = apply_filters('wplms-top-menu',array(
-                                'theme_location'  => 'top-menu',
-                                'container'       => '',
-                                'menu_class'      => 'topmenu',
-                                'fallback_cb'     => 'vibe_set_menu',
-                            ));
 
-                        wp_nav_menu( $args );
                         ?>
                     </div>
                     <div id="vibe_bp_login">
@@ -87,51 +81,46 @@ wp_head();
             </div>
         </div>
         <header>
-            <div class="container">
+            <div class="container-logo">
                 <div class="row">
-                    <div class="col-md-3 col-sm-3 col-xs-12">
                         <?php
-
                             if(is_home()){
                                 echo '<h1 id="logo">';
                             }else{
                                 echo '<h2 id="logo">';
                             }
                         ?>
-                        
-                            <a href="<?php echo vibe_site_url(); ?>"><img src="<?php  echo apply_filters('wplms_logo_url',VIBE_URL.'/images/logo.png'); ?>" alt="<?php echo get_bloginfo('name'); ?>" /></a>
+
+                            <a href="<?php echo home_url();?>/"><img src="<?php echo (isset($logo)?$logo:VIBE_URL.'/images/logo.png'); ?>" alt="<?php echo get_bloginfo('name'); ?>" /></a>
                         <?php
                             if(is_home()){
                                 echo '</h1>';
                             }else{
                                 echo '</h2>';
                             }
-                        ?>
-                    </div>
-                    <div class="col-md-9 col-sm-9">
+                        ?>                     
+                </div>
+            </div>
+            <div class="container">               
+                <div class="row">
+                    <div class="col-xs-12">
                         <div id="searchicon"><i class="icon-search-2"></i></div>
                         <div id="searchdiv">
                             <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
                                 <div><label class="screen-reader-text" for="s">Search for:</label>
-                                    <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="<?php _e('Hit enter to search...','vibe'); ?>" />
-                                    <?php 
-                                        $course_search=vibe_get_option('course_search');
-                                        if(isset($course_search) && $course_search)
-                                            echo '<input type="hidden" value="course" name="post_type" />';
-                                    ?>
+                                    <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="Hit enter to search..." />
                                     <input type="submit" id="searchsubmit" value="Search" />
                                 </div>
                             </form>
                         </div>
                         <?php
-                            $args = apply_filters('wplms-main-menu',array(
+                             wp_nav_menu( array(
                                  'theme_location'  => 'main-menu',
                                  'container'       => 'nav',
                                  'menu_class'      => 'menu',
                                  'walker'          => new vibe_walker,
                                  'fallback_cb'     => 'vibe_set_menu'
-                             ));
-                            wp_nav_menu( $args ); 
+                             ) );
                         ?> 
                     </div>
                     <a id="trigger">
