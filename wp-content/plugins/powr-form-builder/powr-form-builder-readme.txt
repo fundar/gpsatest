c1375b2373b325a3c039ab74ebe5f5520eddcb1c
josechajob <josecha>
2014-11-19 12:50:38 -0600

    fix problem
    fix problem

---
+++ wp-content/plugins/powr-form-builder/powr-form-builder-readme.txt
@@ -0,0 +1,114 @@
+=== Plugin Name ===
+Contributors: powr
+Donate link: http://powr.io/
+Tags: form builder, survey, poll, contact form, mailing list signup, event registration, sales lead, feedback form, mobile form, responsive form, powr, edit in page
+Requires at least: 3.0
+Tested up to: 3.8
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+POWr Form Builder allows you to create and customize beautiful, mobile-responsive forms that you can edit right in the page.
+
+== Description ==
+
+POWr Form Builder allows you to create beautiful, mobile-responsive forms that you can edit **right in the page**. Add the widget to your site, or use the shortcode **[powr-form-builder]** to add forms to any post. 
+
+* Surveys
+* Polls
+* Contact Forms
+* Mailing List Signups
+* Event Registrations
+* Sales Leads
+* Feedback Forms
+* ...much more!
+
+Enter an unlimited number of inputs in the form of text inputs, checkboxes, radio buttons, or select menus. Fully customize all language, fonts, and colors, and receive instant updates when a form is submitted.
+
+The basic version of the POWr Form Builder is free! 
+
+POWr plugins can be used an virtually any website, learn more at www.powr.io
+
+
+Features:
+
+* Unlimited number of form inputs, including text, text-areas, select fields, radio buttons, and checkboxes
+* Immediate email notifications when a form is submitted
+* Custom "thank you" message when form is submitted
+* Option to require users to fill in fields
+* Language support - customize text to use any language
+* Fully customized fonts, colors, borders and background to fit your brand
+
+
+== Installation ==
+
+To install Form Builder:
+
+1. Upload `powr-form-builder.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+To add Form Builder as a widget, go to **Appearance->Widgets**, and drag POWr Form Builder to where you'd like it to appear.
+
+To add Form Builder within a post or a page, simply write the shortcode **[powr-form-builder]** (including the brackets) where you'd like your Form Builder to appear. Then visit your page!
+
+Want a more detailed tutorial? Visit [How to add Form Builder to your Wordpress site](http://powr.io/tutorials/how-to-add-form-builder-plugin-to-your-wordpress-site)
+
+
+== Frequently Asked Questions ==
+
+= How can I add Form Builder to a blog post or page? =
+
+Just add the shortcode **[powr-form-builder]** (including the brackets) in your post where you'd like your Form Builder to appear. Then visit your page!
+
+
+= Why do you require me to sign up? =
+
+Signing up is what allows us to track which widgets are yours, and allows you to edit them right in your page.
+
+
+= Will my users see the settings icon in the corner of the Form Builder? =
+
+No! You only see the settings icon because you own the widget. To temporarily hide it, click on your page and then type "p + down arrow" (the letter p followed by the down arrow). You can show the icon again by typing "p + up-arrow".
+
+
+= Will my users see the settings icon in the corner of the Form Builder? =
+
+No! You only see the settings icon because you own the widget. To temporarily hide this icon, click on your page and then type "p + down-arrow" (the letter p followed by the down arrow). You can show the icon again by typing "p + up-arrow"
+
+
+= I can't see the settings icon anymore, what happened? =
+
+You are just logged out of POWr.io. You can show the icon again by typing "p + up-arrow".
+
+
+= Is Form Builder Free? =
+
+Yes, the basic version of Form Builder is completely free!
+
+
+= Why should I upgrade? =
+
+Upgrading removes all POWr watermarks, speeds up widget loading speed, gets you premium support, and most importantly supports the creation of more great plugins!
+
+
+
+== Screenshots ==
+
+1. Example
+2. Example
+3. Example
+4. Example
+5. Example
+6. Example
+7. Example
+
+== Upgrade Notice ==
+
+= 1.0 =
+First released version.
+
+
+== Changelog ==
+
+= 1.0 =
+* First released version
---
+++ wp-content/plugins/powr-form-builder/powr-form-builder.php
@@ -0,0 +1,117 @@
+<?php
+    /**
+     * @package POWr Form Builder
+     * @version 1.1
+     */
+    /*
+    Plugin Name: POWr Form Builder
+    Plugin URI: http://www.powr.io
+    Description: Create unique, beautiful Forms in seconds! Add the widget to your theme, or create a Form Builder on ANY page or post by using the shortcode [powr-form-builder]. Then, simply visit your site and click the settings icon to customize your Form Builder right in the page. Many more plugins & tutorials at POWr.io.
+    Author: POWr.io
+    Version: 1.1
+    Author URI: http://www.powr.io
+    */
+
+    ///////////////////////////////////////GENERATE JS IN HEADER///////////////////////////////
+    //For local mode (testing)
+    if(!function_exists('powr_local_mode')){
+        function powr_local_mode(){
+          return false;
+        }
+    }
+    //Generates an instance key
+    if(!function_exists('generate_powr_instance')){
+        function generate_powr_instance() {
+          $alphabet = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789';
+          $pass = array(); //remember to declare $pass as an array
+          $alphaLength = strlen($alphabet) - 1; // Put the length -1 in cache.
+          for ($i = 0; $i < 10; $i++) { // Add 10 random characters.
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+          }
+          $pass_string = implode($pass) . time(); // Add the current time to avoid duplicate keys.
+          return $pass_string; // Turn the array into a string.
+        }
+    }
+    //Adds script to the header if necessary
+    if(!function_exists('initialize_powr_js')){
+        function initialize_powr_js(){
+          //No matter what we want the javascript in the header:
+          add_option( 'powr_token', generate_powr_instance(), '', 'yes' );	//Add a global powr token: (This will do nothing if the option already exists)
+          $powr_token = get_option('powr_token'); //Get the global powr_token
+          if(powr_local_mode()){//Determine JS url:
+            $js_url = '//localhost:3000/powr_local.js';
+          }else{
+            $js_url = '//www.powr.io/powr.js';
+          }
+          ?>
+          <script>
+            (function(d){
+              var js, id = 'powr-js', ref = d.getElementsByTagName('script')[0];
+              if (d.getElementById(id)) {return;}
+              js = d.createElement('script'); js.id = id; js.async = true;
+              js.src = '<?php echo $js_url; ?>';
+              js.setAttribute('powr-token','<?php echo $powr_token; ?>');
+              js.setAttribute('external-type','wordpress');
+              ref.parentNode.insertBefore(js, ref);
+            }(document));
+          </script>
+          <?php
+        }
+        //CALL INITIALIZE
+        add_action( 'wp_enqueue_scripts', 'initialize_powr_js' );
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////Create Form Builder widget/////////////////////////////////
+    class Powr_Form_Builder extends WP_Widget{
+      //Create the widget
+      public function __construct(){
+        parent::__construct( 'powr_form_builder',
+                             __( 'POWr Form Builder' ),
+                             array( 'description' => __( 'Form Builder by POWr.io') )
+        );
+      }
+      //This prints the div
+      public function widget( $args, $instance ){
+        $label = $instance['label'];
+        ?>
+        <div class='widget powr-form-builder' label='<?php echo $label; ?>'></div>
+        <?php
+      }
+      public function update( $new_instance, $old_instance ){
+        //TODO: Figure out what needs to happen here
+        $instance = $old_instance;
+        //If no label, then set a label
+        if( empty($instance['label']) ){
+          $instance['label'] = 'wordpress_'.time();
+        }
+        return $instance;
+      }
+      public function form( $instance ){
+        ?>
+        <p>
+          No need to edit here - just click the gears icon on your Form Builder.
+        </p>
+        <p>
+          Learn more at <a href='http://www.powr.io'>POWr.io</a>
+        </p>
+        <?php
+      }
+    }
+    //Register Widget With Wordpress
+    function register_powr_form_builder() {
+      register_widget( 'Powr_Form_Builder' );
+    }
+    //Use widgets_init action hook to execute custom function
+    add_action( 'widgets_init', 'register_powr_form_builder' );
+    //Create short codes for adding plugins anywhere:
+    function powr_form_builder_shortcode( $atts ){
+      if(isset($atts['label'])){
+        $label = $atts['label'];
+      }else{
+        $label = '';
+      }
+      return "<div class='powr-form-builder' label='$label'></div>";
+    }
+    add_shortcode( 'powr-form-builder', 'powr_form_builder_shortcode' );
+  ?>
