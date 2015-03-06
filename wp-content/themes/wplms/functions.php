ed1c4575f98121fa663a8d7afe636b6efd75e5dc
josechajob <josecha>
2015-03-06 16:39:58 -0600

    test redirections

--- wp-content/themes/wplms/functions.php
+++ wp-content/themes/wplms/functions.php
@@ -245,11 +245,9 @@ function no_sidebars() {
 
 function buddypress_login_redirection($redirect_url,$request_url,$user){
     global $bp;
-     $custom_redirect_url="http://gpsatest.fundarlabs.mx/members";
-        return $custom_redirect_url; 
-} 
-add_filter('login_redirect',array($this, 'buddypress_login_redirection'),100,3);  
- 
+     $custom_redirect_url= '/members-2/'.bp_core_get_user_domain($user->ID).'/';
+        return 
+}  
 
 add_filter('thesis_show_sidebars', 'no_sidebars');
  
