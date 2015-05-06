f772a4c6cb3aa1204553f5cb6f8a5a78993aba8e
josechajob <josecha>
2015-05-06 18:06:24 -0500

    remove

--- wp-content/themes/wplms/functions.php
+++ wp-content/themes/wplms/functions.php
@@ -252,8 +252,3 @@ add_action( 'bp_member_header_actions', 'bp_add_friend_button' );
 add_action( 'bp_member_header_actions', 'bp_send_private_message_button' );
 
 
-function cc_custom_excerpt_length() {
-return '3';
-}
-add_filter( 'bp_activity_excerpt_length', 'cc_custom_excerpt_length' );
-
