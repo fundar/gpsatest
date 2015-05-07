7320a8b74fe7f2a8b7a6bc99a83ef1c3f1cc84c6
josechajob <josecha>
2015-05-07 17:55:13 -0500

    add

--- wp-content/themes/wplms/functions.php
+++ wp-content/themes/wplms/functions.php
@@ -258,7 +258,7 @@ return '180';
 
 
 function cc_excerpt_append_text() {
-return '<a href="http://gpsatest.fundarlabs.mx/networking/" rel="nofollow">[Read more]</a>';
+return 'Read more in networking';
 }
 
 add_filter('thesis_show_sidebars', 'no_sidebars');
