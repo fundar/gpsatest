aa1e5b20fc6c64ae2be1db4c1eb891d4751d8f52
josechajob <josecha>
2015-06-24 10:45:16 -0500

    reparando error

--- wp-content/themes/wplms/members/members-loop.php
+++ wp-content/themes/wplms/members/members-loop.php
@@ -22,11 +22,11 @@ isset($loop_number)?$loop_number:$loop_number=5;
 <?php do_action( 'bp_before_members_loop' ); ?>
 
 
-<?php if ( bp_ajax_querystring( 'members' ) ==""){
+<?php if ( bp_ajax_querystring( 'members' ) ==""){ ?>
 
 <?php if ( bp_has_members( bp_ajax_querystring( 'members' ).'&type=alphabetical'.'&per_page='.$loop_number ) ) : ?>
 
-}else{
+<?php }else{ <?
 
 <?php if ( bp_has_members( bp_ajax_querystring( 'members' ).'&per_page='.$loop_number ) ) : ?>
 
