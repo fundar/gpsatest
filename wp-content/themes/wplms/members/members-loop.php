8854b7f7589caa90b605dc473f78a29ba9710ec7
josechajob <josecha>
2015-06-24 10:42:50 -0500

    solo buscar alphabetics = nada

--- wp-content/themes/wplms/members/members-loop.php
+++ wp-content/themes/wplms/members/members-loop.php
@@ -22,9 +22,15 @@ isset($loop_number)?$loop_number:$loop_number=5;
 <?php do_action( 'bp_before_members_loop' ); ?>
 
 
+<?php if ( bp_ajax_querystring( 'members' ) ==""){
 
 <?php if ( bp_has_members( bp_ajax_querystring( 'members' ).'&type=alphabetical'.'&per_page='.$loop_number ) ) : ?>
 
+}else{
+
+<?php if ( bp_has_members( bp_ajax_querystring( 'members' ).'&per_page='.$loop_number ) ) : ?>
+
+}
 
 
 	<div id="pag-top" class="pagination">
